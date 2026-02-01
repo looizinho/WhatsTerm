3a810014ea2bd8ba2a0a1b4287bcfd8e186226a8
Luizinho Neto <lmoraes@me.com>
2026-02-01 00:45:45 -0300

    First Commit.

---
+++ prisma/migrations/20260201032816_init/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "Conversation" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "from" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "conversationId" TEXT NOT NULL,
+    "direction" TEXT NOT NULL,
+    "text" TEXT,
+    "rawPayload" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Message_conversationId_fkey" FOREIGN KEY ("conversationId") REFERENCES "Conversation" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
